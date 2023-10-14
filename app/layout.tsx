f764995baa9d4d9f7389db52ad1b9b182dff2703
1p22geo <1p22geodecki@gmail.com>
2023-10-14 21:30:51 +0200

    format: Formatted the files for Prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  
   return (
     <html lang="en">
       <body>{children}</body>
