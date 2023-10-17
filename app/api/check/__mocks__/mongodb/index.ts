4679adef31a1fae15d399bfe04281a2e64bf35a0
1p22geo <1p22geodecki@gmail.com>
2023-10-18 01:41:59 +0200

    test: Finished the test for /api/check .
    Refs: WB-27

--- app/api/check/__mocks__/mongodb/index.ts
+++ app/api/check/__mocks__/mongodb/index.ts
@@ -1,14 +1,15 @@
 const { ObjectId: RealObjectId } = jest.requireActual("mongodb");
 
 class MongoClient {
-  constructor() {
-    console.log("Creating client");
-  }
-  close() {
-    console.log("Closing client");
-  }
-  connect() {
-    console.log("Creating client");
+  close: () => void;
+  connect: () => void;
+  create: (_str: string) => void;
+  constructor(str: string) {
+    this.create = jest.fn();
+    this.close = jest.fn();
+    this.connect = jest.fn();
+    this.create(str);
+    console.info(str);
   }
   db() {
     return {
@@ -16,27 +17,41 @@ class MongoClient {
         switch (coll) {
           case "sessions":
             return {
-              findOne() {
-                return {
-                  _id: new RealObjectId("652eb26557e45bcc221d51d5"),
-                  user: new RealObjectId("652eb25c57e45bcc221d51d4"),
-                  started: 1697559141835,
-                  duration: 300000,
-                };
-              },
+              findOne: jest.fn((q) => {
+                if (q._id.toString() === "652eb26557e45bcc221d51d5")
+                  return {
+                    _id: new RealObjectId("652eb26557e45bcc221d51d5"),
+                    user: new RealObjectId("652eb25c57e45bcc221d51d4"),
+                    started: 1697559141835,
+                    duration: 300000,
+                  };
+                if (q._id.toString() === "652eb26557e45bcc221d51d8")
+                  return {
+                    _id: new RealObjectId("652eb26557e45bcc221d51d8"),
+                    user: new RealObjectId("652eb25c57e45bcc221d5000"),
+                    started: 1697559141835,
+                    duration: 300000,
+                  };
+                if (q._id.toString() === "652eb26557e45bcc221d51ff") throw Error("test error please ignore");
+
+                return null;
+              }),
             };
           case "users":
             return {
-              findOne() {
-                return {
-                  _id: new RealObjectId("652eb25c57e45bcc221d51d4"),
-                  email: "1p22geo@gmail.com",
-                  name: "Bartosz Geodecki",
-                  hash: "489cd5dbc708c7e541de4d7cd91ce6d0f1613573b7fc5b40d3942ccb9555cf35",
-                  added: 1697559132609,
-                  type: "user",
-                };
-              },
+              findOne: jest.fn((q) => {
+                if (q._id.toString() === "652eb25c57e45bcc221d51d4")
+                  return {
+                    _id: new RealObjectId("652eb25c57e45bcc221d51d4"),
+                    email: "1p22geo@gmail.com",
+                    name: "Bartosz Geodecki",
+                    hash: "489cd5dbc708c7e541de4d7cd91ce6d0f1613573b7fc5b40d3942ccb9555cf35",
+                    added: 1697559132609,
+                    type: "user",
+                  };
+
+                return null;
+              }),
             };
         }
       },
--- app/api/check/route.test.ts
+++ app/api/check/route.test.ts
@@ -7,6 +7,10 @@ import { POST } from "./route";
 
 jest.mock("mongodb");
 
+beforeAll(() => {
+  console.info = jest.fn();
+});
+
 describe("/api/check", () => {
   it("Checks a correct session", async () => {
     const { req } = createMocks({
@@ -17,5 +21,51 @@ describe("/api/check", () => {
     const response = await POST(req as unknown as Request);
 
     expect(response.status).toBe(200);
+  });
+  it("Checks a nonexistent session", async () => {
+    const { req } = createMocks({
+      method: "POST",
+      body: { session: "652eb26557e45bcc221d0000" },
+    });
+    req.json = jest.fn().mockResolvedValue(req.body);
+    const response = await POST(req as unknown as Request);
+
+    expect(response.status).toBe(404);
+  });
+  it("Checks a nonexistent user", async () => {
+    const { req } = createMocks({
+      method: "POST",
+      body: { session: "652eb26557e45bcc221d51d8" },
+    });
+    req.json = jest.fn().mockResolvedValue(req.body);
+    const response = await POST(req as unknown as Request);
+
+    expect(response.status).toBe(501);
+  });
+  it("reacts to unexpected errors", async () => {
+    const { req } = createMocks({
+      method: "POST",
+      body: { session: "652eb26557e45bcc221d51ff" },
+    });
+    console.error = jest.fn();
+    req.json = jest.fn().mockResolvedValue(req.body);
+    const response = await POST(req as unknown as Request);
+
+    expect(response.status).toBe(400);
+    expect(console.error).toBeCalledWith(Error("test error please ignore"));
+  });
+  it("reacts to lack of environment variables", async () => {
+    const { req } = createMocks({
+      method: "POST",
+      body: { session: "652eb26557e45bcc221d51ff" },
+    });
+    console.error = jest.fn();
+    req.json = jest.fn().mockResolvedValue(req.body);
+    process.env = { ...process.env, MONGO_URI: "yes." };
+    const response = await POST(req as unknown as Request);
+
+    expect(response.status).toBe(400);
+    expect(console.error).toBeCalledWith(Error("test error please ignore"));
+    expect(console.info).toBeCalledWith("yes.");
   });
 });
