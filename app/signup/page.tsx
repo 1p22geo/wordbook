f00361660095b1487061091ab98ecc97df7709dd
Geodecki 2P gr.1 <113300767+1p22geo@users.noreply.github.com>
2023-10-20 20:02:56 +0200

    merge: responsibility fixes
    * ui: Made the app a bit more responsive
    
    * format: Fixed formatting for Prettier
    
    * ci: These workflows should have never been called on PRs
    
    
    Refs: WB-38

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -10,7 +10,7 @@ const Page = () => {
   return (
     <>
       <div className="grid h-screen w-screen place-content-center bg-secondary-200">
-        <div className="flex flex-col items-center rounded-xl bg-primary-200 p-16 text-center">
+        <div className="m-4 flex flex-col items-center rounded-xl bg-primary-200  p-8 text-center">
           <div className="mb-8">
             <h1 className="text-4xl">Welcome to Wordbook</h1>
             <h2 className="text-lg">We&apos;re sure you&apos;ll like it here</h2>
