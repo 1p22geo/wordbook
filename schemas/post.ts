5b4a6433eaa0399c7f5d4bcb2e36a9e904a242c9
1p22geo <1p22geodecki@gmail.com>
2023-10-21 09:35:09 +0200

    format: fixed formatting for Prettier

--- schemas/post.ts
+++ schemas/post.ts
@@ -1,8 +1,8 @@
 import { ObjectId, WithId } from "mongodb";
 
-export interface Post{
-    author:ObjectId;
-    posted:number;
-    content:string;
+export interface Post {
+  author: ObjectId;
+  posted: number;
+  content: string;
 }
 export interface PostID extends WithId<Post> {}
