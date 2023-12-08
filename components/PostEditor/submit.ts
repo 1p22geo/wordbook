b36be6e94d224d5cfaeb50acd3aea65c1e4cab62
Geodecki 2P gr.1 <113300767+1p22geo@users.noreply.github.com>
2023-12-08 08:26:13 +0000

    fix: patched the regex

--- components/PostEditor/submit.ts
+++ components/PostEditor/submit.ts
@@ -2,7 +2,7 @@ import { Dispatch, SetStateAction } from "react";
 import { alertMessage } from "components/Alert";
 
 export async function submit(post: string, setalert: Dispatch<SetStateAction<alertMessage>>) {
-  const npost = post.replace(/```KaTeX((.|\n|\r)*?)```/gs, "$$$ $1 $$$");
+  const npost = post.replace(/```KaTeX(([^\n\r]|\n|\r)*?)```/gs, "$$$ $1 $$$");
   const res = await fetch("/api/post", { method: "POST", body: JSON.stringify({ content: npost }) });
   if (res.ok) window.location.reload();
   else setalert({ type: "error", message: "Something has gone wrong" });
