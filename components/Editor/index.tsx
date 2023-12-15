d5a14d1dcde2c6645a94e80bd0d9315c7a42226c
Geodecki 2P gr.1 <113300767+1p22geo@users.noreply.github.com>
2023-12-15 15:44:37 +0000

    ?

--- components/Editor/index.tsx
+++ components/Editor/index.tsx
@@ -21,10 +21,9 @@ export const Editor = ({ value, setValue }: { value: string; setValue: Dispatch<
         }}
         previewOptions={{
           components: {
-            code: ({ inline, children, className, ...props }) => {
+            code: ({ children, className, ...props }) => {
               if (!children) return;
               const txt = children[0] || "";
-              if (inline) {
                 if (typeof txt === "string" && /^\$\$(.*)\$\$/.test(txt)) {
                   const html = katex.renderToString(txt.replace(/^\$\$(.*)\$\$/, "$1"), {
                     throwOnError: false,
@@ -32,7 +31,6 @@ export const Editor = ({ value, setValue }: { value: string; setValue: Dispatch<
                   return <code dangerouslySetInnerHTML={{ __html: html }} />;
                 }
                 return <code>{txt}</code>;
-              }
               const code =
                 props.node && props.node.children
                   ? getCodeString(props.node.children as unknown as ElementContent[])
