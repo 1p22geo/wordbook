beb0eafc7a5e99cb3f928e6c818767cbe42f420d
Geodecki 2P gr.1 <113300767+1p22geo@users.noreply.github.com>
2023-12-20 08:35:41 +0000

    fix: no longer importing mongo in frontend

--- components/Post/index.stories.tsx
+++ components/Post/index.stories.tsx
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { ObjectId } from "mongodb";
 import { Post } from ".";
 
 const meta: Meta<typeof Post> = {
@@ -7,14 +6,14 @@ const meta: Meta<typeof Post> = {
   component: Post,
   args: {
     post: {
-      _id: new ObjectId("876587658765876587658765"),
+      _id: "876587658765876587658765" as any,
       author: {
         email: "user@email.com",
         name: "Random user",
         hash: "x",
         added: 0,
         type: "user",
-        _id: new ObjectId("876587658765876587658765"),
+        _id: "876587658765876587658765" as any,
       },
       posted: Date.now() - 3600,
       content: "Good morning everyone",
