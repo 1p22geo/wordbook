993bb8d9154b0a3404587241519ecf9d5bc82a2a
1p22geo <1p22geodecki@gmail.com>
2023-12-23 10:30:01 +0000

    e2e: added chrome for e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 0 : 0,
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -67,10 +67,10 @@ export default defineConfig({
     //   name: "Microsoft Edge",
     //   use: { ...devices["Desktop Edge"], channel: "msedge" },
     // },
-    // {
-    //   name: "Google Chrome",
-    //   use: { ...devices["Desktop Chrome"], channel: "chrome" },
-    // },
+     {
+       name: "Google Chrome",
+       use: { ...devices["Desktop Chrome"], channel: "chrome" },
+     },
   ],
 
   /* Run your local dev server before starting the tests */
